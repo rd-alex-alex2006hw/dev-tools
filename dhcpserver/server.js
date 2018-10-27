544c2bc556e8b5db3d50e48101c8fd9d03f579c0
Alex Davis <alex2006hw@gmail.com>
2018-10-27 08:51:28 -0700

    update-squid

--- dhcpserver/server.js
+++ dhcpserver/server.js
@@ -1,9 +1,9 @@
 var dhcpd = require('dhcp');
 ///////////////////////////////
-var INTERFACE='192.168.2.1'
-var RANGE = [ "192.168.2.100", "192.168.2.200"]
+var INTERFACE='192.168.3.1'
+var RANGE = [ "192.168.3.100", "192.168.3.200"]
 var STATIC = {
-//   "00:1C:C2:17:B2:F2": "192.168.2.11"
+   "0:1c:c2:17:b2:f3": "192.168.3.11"
 }
 var HOST_COUNTER = 0
 ///////////////////////////////
@@ -15,7 +15,7 @@ var s = dhcpd.createServer({
   // Option settings
   netmask: '255.255.255.0',
   router: [ INTERFACE ],
-  dns: ["8.8.8.8", "8.8.4.4"],
+  dns: ["75.75.75.75", "8.8.8.8"],
   server: INTERFACE, // This is us
   hostname: hostname()
 });
